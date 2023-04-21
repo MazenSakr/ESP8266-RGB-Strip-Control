40251dcec4b66c173e3932dd07524b7deb5fb8ea
Mazen Sakr <mazen_sakr@outlook.com>
2023-04-21 06:46:26 +0200

    project v1 completion

--- ESP8266_wifi_controlled_car/main.py
+++ ESP8266_wifi_controlled_car/main.py
@@ -1,43 +1,46 @@
 import network
-import socket
+try:
+  import usocket as socket
+except:
+  import socket
 import machine
-
+import gc
+gc.collect()
 # Set up access point
 ap = network.WLAN(network.AP_IF)
 ap.active(True)
-ap.config(essid='car1', authmode=network.AUTH_WPA_WPA2_PSK, password='mazenscar')
+ap.config(essid='car2', authmode=network.AUTH_WPA_WPA2_PSK, password='mazenscar2')
+ap.ifconfig(('192.168.1.4', '255.255.255.0', '192.168.1.4', '8.8.8.8'))
 print('Access Point created successfully')
 print(ap.ifconfig())
 
 
 # Set up web server
-html = """<!DOCTYPE html>
+html = '''<!DOCTYPE html>
 <html>
     <head> <title>Car 1</title> </head>
     <body bgcolor="#808080">
-        <h1 style="text-align:center">ESP8266 Car Control</h1>
-        <div style="text-align:center">
-            <button style="background-color:#009FF9; color:white; font-size:60px" onclick="move('forward')">↑</button>
-            <br><br>
-            <button style="background-color:#009FF9; color:white; font-size:60px; margin-right:10px" onclick="move('left')">←</button>
-	    <>
-            <button style="background-color:#009FF9; color:white; font-size:60px; margin-left:10px" onclick="move('right')">→</button>
-            <br><br>
-            <button style="background-color:#009FF9; color:white; font-size:60px" onclick="move('backward')">↓</button>
-        </div>
-
         <script>
             function move(direction) {
                 var xhr = new XMLHttpRequest();
-                xhr.open("GET", "/?direction="+direction, true);
+                xhr.open("GET", "http://192.168.1.4/?direction="+direction, true);
                 xhr.send();
             }
         </script>
+        <div style="text-align:center">
+            <button style="background-color:#009FF9; color:white; font-size:60px" onclick="move('forward')">UP</button>
+            <br><br>
+            <button style="background-color:#009FF9; color:white; font-size:60px; margin-right:10px" onclick="move('left')" >LEFT</button>
+            <button style="background-color:#009FF9; color:white; font-size:60px; margin-left:10px" onclick="move('stop')" >STOP</button>
+            <button style="background-color:#009FF9; color:white; font-size:60px; margin-left:10px" onclick="move('right')" >RIGHT</button>
+            <br><br>
+            <button style="background-color:#009FF9; color:white; font-size:60px" onclick="move('backward')" >DOWN</button>
+        </div>
     </body>
-</html>"""
+</html>'''
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', 53))
+s.bind(('192.168.1.4', 80))
 s.listen(5)
 
 # Set up motor driver
@@ -79,7 +82,8 @@ def stop():
 while True:
     conn, addr = s.accept()
     print('Got a connection from %s' % str(addr))
-    
+    response = html
+    conn.send(response.encode())
     request = conn.recv(1024).decode()
     
     direction = None
@@ -94,11 +98,10 @@ while True:
             move_left()
         elif direction == 'right':
             move_right()
-        else:
+        elif direction == 'stop':
             stop()
 
-        response = html
-        conn.send(response.encode())
+    
     
     conn.close()
 
---
+++ ESP8266_wifi_controlled_car/notes.md
@@ -0,0 +1,20 @@
+#contains commands to manipulate code and flash esp and all notes and pitfalls
+# Flashing:
+1. Connect ESP8266 to PC via USB
+2. use command: esptool --port <your comm port for windows> erase_flash #to erase flash
+3. use command: esptool --port <your comm port for windows> --baud 460800 write_flash --flash_size=detect -fm dout 0 .\esp8266-20220618-v1.19.1.bin #path to bin file #to flash esp8266 with micropython
+note: for non nodemcu esp boards remove -fm dout 0, or if command is not working, to test after flash micropython should start an access point with ssid: MicroPython-xxxxxx and password: 12345678 where xxxxxx is the last 6 digits of the MAC address of the ESP8266, then connect with it using tera term to control it using built in repl(python shell)
+4. use command: ampy --port <your comm port for windows> put main.py #to upload main.py to esp8266
+note: main.py is run after boot.py on esp startup
+5. to remove main.py from esp8266 use command: ampy --port <your comm port for windows> rm main.py
+or alternatively use command: ampy --port <your comm port for windows> rmdir / #to remove all files from esp8266
+or alternatively open repl and import os and use os.remove('main.py') to remove main.py and os.listdir() to list files on esp8266.
+
+
+# connections and pinouts:
+To flash bare esp8266 modules : https://www.hackster.io/brian-lough/3-simple-ways-of-programming-an-esp8266-12x-module-c514ee
+nodemcu pinout: https://www.instructables.com/id/ESP8266-NodeMCU-12E-Pinout/
+esp8266 micropython reference:  https://docs.micropython.org/en/latest/esp8266/tutorial/intro.html
+circuitry: power(18650 batteries) ->5v regulator -> 3.3v regulator -> esp8266 -> motor driver (control)
+                                 |                                |-> led(system on indicator)
+                                 |->(L298N)Motor driver -> motors (power)   
