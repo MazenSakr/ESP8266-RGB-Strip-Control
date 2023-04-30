f20b2014c1f9bc6a50e87edb52b1b9b21e163b8d
Mazen Sakr <mazen_sakr@outlook.com>
2023-04-30 09:03:28 +0300

    working code after coroutines and threads failure

--- ESP8266_wifi_controlled_RGB_strip/main.py
+++ ESP8266_wifi_controlled_RGB_strip/main.py
@@ -3,147 +3,146 @@ import time
 import usocket as socket
 import network
 from machine import Pin, PWM
-import _thread
+import uasyncio
 import random
+import color
+import sys
 
-# Set up access point
-ap = network.WLAN(network.AP_IF)
-ap.active(True)
-ap.config(essid='TeamRGB', authmode=network.AUTH_OPEN)
-ap.ifconfig(('192.168.1.4', '255.255.255.0', '192.168.1.4', '8.8.8.8'))
-print(ap.ifconfig())
+REDPIN = 5
+GREENPIN = 14
+BLUEPIN = 12
 
-# Set up web server
-def handle_request(request):
-    request = str(request)
-    if 'GET /rgb?' in request:
-        # Parse RGB values from request
-        start_index = request.index('GET /rgb?') + 8
-        end_index = request.index(' HTTP')
-        rgb_values = request[start_index:end_index].split('&')
-        r = int(rgb_values[0].split('=')[1],16)
-        g = int(rgb_values[1].split('=')[1],16)
-        b = int(rgb_values[2].split('=')[1],16)
-        mode = int(rgb_values[3].split('=')[1])
-        
-        # Set RGB values and mode
-        set_rgb(r, g, b)
-        set_mode(mode, r, g, b)
-        
-def set_rgb(r, g, b):
-    # Set PWM duty cycle for each color
-    global r_pwm, g_pwm, b_pwm
-    r_pwm.duty(r)
-    g_pwm.duty(g)
-    b_pwm.duty(b)
+class RGBStripControl():
 
-def set_mode(mode, r, g, b):
-    # Set mode for LED strip operation
-    threadStatus = False
-    time.sleep(0.01)
-    threadStatus = True
-    if mode == 0:
-        pass
-    elif mode == 1:
-        _thread.start_new_thread(brightness_cycle, (r, g, b))
-    elif mode == 2:
-        _thread.start_new_thread(rainbow_cycle, (r, g, b))
-    elif mode == 3:
-        _thread.start_new_thread(bounce, (r, g, b))
-    else:
-        print("Invalid mode")
-
-def brightness_cycle(r, g, b):
-    # Cycle between 0 and full brightness of selected color
-    global threadStatus
-    Hue,Saturation,Value = RGBToHSV(r, g, b)
-    brightness = 0
-    while threadStatus :
-        for i in range(Value+1):
-            brightness = i
-            r,g,b = HSVToRGB(Hue, Saturation, brightness)
-            set_rgb(int(r),int(g),int(b))
-            time.sleep(0.01)
-        for i in range(Value+1):
-            brightness = 255-i
-            r,g,b = HSVToRGB(Hue, Saturation, brightness)
-            set_rgb(int(r),int(g),int(b))
-            time.sleep(0.01)
+    def __init__(self):
+        self.color = color.Color(20,20,20)
+        self.mode = 0
+        self.startAP()
+        self.setupServer()
+        self.setupPins()
+        self.set_rgb()
+        self.handle_request()
+        #uasyncio.run(self.start_coro())
 
 
-def rainbow_cycle(r, g, b):
-    # Cycle between colors of the rainbow
-    global threadStatus
-    Hue,Saturation,Value = RGBToHSV(r, g, b)
-    delay = Value/2550
-    while threadStatus:
-        for i in range(360):
-            Hue = i
-            r,g,b = HSVToRGB(Hue, Saturation, Value)
-            set_rgb(int(r),int(g),int(b))
+    def startAP(self):
+        # Set up access point
+        self.accessPoint = network.WLAN(network.AP_IF)
+        self.accessPoint .active(True)
+        self.accessPoint .config(essid='TeamRGB', authmode=network.AUTH_OPEN)
+        self.accessPoint .ifconfig(('192.168.1.4', '255.255.255.0', '192.168.1.4', '8.8.8.8'))
+        print(self.accessPoint .ifconfig())
+    
+    def setupServer(self):
+        # Start web server
+        self.websock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.websock.setblocking(False)
+        self.websock.settimeout(0.1)
+        self.websock.bind(('', 80))
+        self.websock.listen(5)
+
+    # async def start_coro(self):
+    #     print("loop started")
+    #     self.loop1 = uasyncio.get_event_loop()
+    #     self.loop1.run_forever()
+    #     while True:
+    #         try :
+    #             uasyncio.wait_for_ms(self.handle_request(), 100)
+    #         except:
+    #             pass
+    #         await uasyncio.sleep_ms(1000)
+
+    def setupPins(self):
+        # Set up RGB LED strip control using transistors connected to ESP pins
+        self.r_pin = Pin(REDPIN, Pin.OUT)
+        self.g_pin = Pin(GREENPIN, Pin.OUT)
+        self.b_pin = Pin(BLUEPIN, Pin.OUT)
+        self.r_pwm = PWM(self.r_pin)
+        self.g_pwm = PWM(self.g_pin)
+        self.b_pwm = PWM(self.b_pin)
+        self.r_pwm.freq(100) # Set PWM frequency to 1kHz
+        self.g_pwm.freq(100) # Set PWM frequency to 1kHz
+        self.b_pwm.freq(100) # Set PWM frequency to 1kHz
+
+    def handle_request(self):
+        while True:
+            # Accept incoming connection
+            try:
+                self.connection, _ = self.websock.accept()
+                request = self.connection.recv(1024)
+                print(request)
+                self.connection.close()
+                request = str(request)
+                if 'GET /rgb?' in request:
+                    # Parse RGB values from request
+                    start_index = request.index('GET /rgb?') + 8
+                    end_index = request.index(' HTTP')
+                    rgb_values = request[start_index:end_index].split('&')
+                    self.color.red = int(rgb_values[0].split('=')[1],16)
+                    self.color.green = int(rgb_values[1].split('=')[1],16)
+                    self.color.blue = int(rgb_values[2].split('=')[1],16)
+                    self.mode = int(rgb_values[3].split('=')[1])
+            except:
+                pass
+            # Set RGB values and mode
+            self.set_rgb()
+            self.set_mode()
+            
+    def set_rgb(self):
+        # Set PWM duty cycle for each color
+        self.r_pwm.duty(self.color.red*5)
+        self.g_pwm.duty(self.color.green*5)
+        self.b_pwm.duty(self.color.blue*5)
+
+    def set_mode(self):
+        # Set mode for LED strip operation
+        if self.mode == 0:
+            pass
+        elif self.mode == 1:
+            self.brightness_cycle()
+        elif self.mode == 2:
+            self.rainbow_cycle()
+        elif self.mode == 3:
+            self.bounce()
+        else:
+            print("Invalid mode")
+
+    def brightness_cycle(self):
+        # Cycle between 0 and full brightness of selected color
+        self.color.convHSV()
+        brightness = self.color.Value
+        while True :
+            for i in range(brightness+1):
+                self.color.Value = i
+                self.color.convRGB()
+                self.set_rgb()
+                time.sleep(0.01)
+            for i in range(brightness+1):
+                self.color.Value = 255 - i
+                self.color.convRGB()
+                self.set_rgb()
+                time.sleep(0.01)
+
+    def rainbow_cycle(self):
+        # Cycle between colors of the rainbow
+        self.color.convHSV()
+        delay = self.color.Value/2550
+        while True:
+            for i in range(360):
+                self.color.Hue = i
+                self.color.convRGB()
+                self.set_rgb()
+                time.sleep(delay)
+
+    def bounce(self):
+        # Bounce between different colors
+        self.color.convHSV()
+        delay = self.color.Value/2550
+        while True:
+            self.color.red = random.randint(0, 255)
+            self.color.green = random.randint(0, 255)
+            self.color.blue = random.randint(0, 255)
             time.sleep(delay)
 
-def bounce(r, g, b):
-    # Bounce between different colors
-    global threadStatus
-    Hue,Saturation,Value = RGBToHSV(r, g, b)
-    delay = Value/2550
-    while threadStatus:
-        r = random.randint(0, 255)
-        g = random.randint(0, 255)
-        b = random.randint(0, 255)
-        time.sleep(delay)
+Stripcontroller = RGBStripControl()
 
-def RGBToHSV(red,green,blue):
-    Value = max(red, green, blue)
-    min_value = min(red, green, blue)
-    Saturation = (Value - min_value) / Value
-    Hue = 0 if Value == red == green == blue  else \
-        60 * (green - blue) / (Value - min_value) if Value == red != green != blue else \
-        60 * (blue - red) / (Value - min_value) + 120 if Value == green != red != blue else \
-        60 * (red - green) / (Value - min_value) + 240 if Value == blue != red != green else \
-        60 * ((green - red) / (Value - min_value) + 2) if Value != red != green != blue else None
-    return Hue, Saturation, Value
-
-def HSVToRGB(Hue, Saturation, Value):
-    Chroma = Value * Saturation
-    X = Chroma * (1 - abs((Hue / 60) % 2 - 1))
-    m = Value - Chroma
-    (Red, Green, Blue) = (Chroma, X, 0) if 0 <= Hue < 60 else \
-           (X, Chroma, 0) if 60 <= Hue < 120 else \
-           (0, Chroma, X) if 120 <= Hue < 180 else \
-           (0, X, Chroma) if 180 <= Hue < 240 else \
-           (X, 0, Chroma) if 240 <= Hue < 300 else \
-           (Chroma, 0, X)
-    (Red, Green, Blue) = ((Red + m) * 255, (Green + m) * 255, (Blue + m) * 255)
-    return Red, Green, Blue
-
-
-# Set up RGB LED strip control using transistors connected to ESP pins
-r_pin = Pin(5) # Replace with actual pin number
-g_pin = Pin(4) # Replace with actual pin number
-b_pin = Pin(0) # Replace with actual pin number
-
-r_pwm = PWM(r_pin)
-g_pwm = PWM(g_pin)
-b_pwm = PWM(b_pin)
-
-r_pwm.freq(1000) # Set PWM frequency to 1kHz
-g_pwm.freq(1000) # Set PWM frequency to 1kHz
-b_pwm.freq(1000) # Set PWM frequency to 1kHz
-
-
-
-# Start web server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', 80))
-s.listen(5)
-
-threadStatus = True
-set_rgb(255,255,255)
-
-while True:
-    conn, addr = s.accept()
-    request = conn.recv(1024)
-    handle_request(request)
-    conn.close()
